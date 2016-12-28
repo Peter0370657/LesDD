f3dc3b908315ac8461f4172862b818285bd4a82a
Peter Leriche <R0370657@student.thomasmore.be>
2016-12-28 22:53:45 +0100

    Prototypes gemaakt

--- main.js
+++ main.js
@@ -5,4 +5,36 @@ var dal = require('./storage.js');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";   // moet erbij zodat het geen problemen vormt om een self-signed cert te negeren.
 
 
+
+var Drone = function (){
+    this._id = id;
+    this.name = name;
+    this.mac = mac;
+    this.location = location;
+};
+var Content = function (){
+    this._id = id;
+    this.mac_address = mac_address;
+    this.datetime = datetime;
+    this.rssi = rssi;
+    this.ref = ref;
+    this.url = url
+};
+var File = function (){
+    this._id = id;
+    this.date_first_record = date_first_record;
+    this.date_last_record = date_last_record;
+    this.date_loaded = date_loaded;
+    this.contents_count = contents_count;
+};
+
+
+
+/*
+dal.ClearFile();
+dal.ClearDrone();
+dal.ClearContent();
+*/
+
+
 console.log("Hello World!"); 
