4067a84cecdb714ecea230763896239a1a0cba51
Peter Leriche <R0370657@student.thomasmore.be>
2016-12-28 21:15:04 +0100

    Main.js even leeggemaakt om van 0 te beginnen, storage.js gevult met de code die we laatste les gezamelijk hebben gemaakt als voorbeeld

--- storage.js
+++ storage.js
@@ -1,10 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017/prober';
+var url = 'mongodb://localhost:27017/booksdb';
 // mongo in nodeJs: http://mongodb.github.io/node-mongodb-native/2.2/
 // mongo API : http://mongodb.github.io/node-mongodb-native/2.2/api/Collection.html
 
 var dal = {
+
 	connect: function (err, result) {
 		MongoClient.connect(url, function (error, db) {
 			if (error)
@@ -13,19 +14,19 @@ var dal = {
 			result(db);
 		});
 	},
-	clearDrone: function (call) {
+	listBooks: function (callback) {
 		this.connect(null, function (db) {
-			db.collection('drones').drop(function (err, result) {
-				//callback(result);
+			db.collection('books').find({}).toArray(function (err, result) {
 				db.close();
+                                callback(result);
 			});
-		})
+		});
 	},
-	insertDrone: function (drone, callback) {
+	insertBooks: function (book, callback) {
 		this.connect(null, function (db) {
-			db.collection('drones').insert(drone, function (err, result) {
-				//callback(result);
+			db.collection('books').insert(book, function (err, result) {
 				db.close();
+                                callback();
 			});
 		});
 	}
