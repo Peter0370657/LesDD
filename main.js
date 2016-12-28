acd913897d1969e7fb0b0cfc3588ba558be1ea55
Peter Leriche <R0370657@student.thomasmore.be>
2016-12-28 23:00:13 +0100

    prototypes uitgebreid

--- main.js
+++ main.js
@@ -6,26 +6,35 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";   // moet erbij zodat het geen p
 
 var beginlink = "https://web-ims.thomasmore.be/datadistribution/API/2.0";
 
-var Drone = function (id, name, mac, location){
+var Drone = function (id, name, mac, location, last_packet_date, files, files_count){
     this._id = id;
     this.name = name;
     this.mac = mac;
     this.location = location;
+    this.last_packet_date= last_packet_date;
+    this.files = files;
+    this.files_count = files_count;
 };
-var Content = function (id, mac_address, datetime, rssi, ref, url){
+var Content = function (id, mac_address, datetime, rssi, ref, url, droneId, fileId){
     this._id = id;
     this.mac_address = mac_address;
     this.datetime = datetime;
     this.rssi = rssi;
     this.ref = ref;
     this.url = url;
+    this.droneId = droneId;
+    this.fileId = fileId;
 };
-var File = function (id, date_first_record, date_last_record, date_loaded, contents_count){
+var File = function (id, date_first_record, date_last_record, date_loaded, contents_count, url, ref, contents, droneId){
     this._id = id;
     this.date_first_record = date_first_record;
     this.date_last_record = date_last_record;
     this.date_loaded = date_loaded;
     this.contents_count = contents_count;
+    this.url = url;
+    this.ref= ref;
+    this.contents = contents;
+    this.droneId = droneId;
 };
 
 var settings  = function (url){
