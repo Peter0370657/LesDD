33edada9c1bee045bf9b47291092cc3686f200c4
Peter Leriche <R0370657@student.thomasmore.be>
2016-12-28 21:30:23 +0100

    instaleren van packages liep wat moeizaam aangezien file niet op c schijf staat en cd parameter niet wou werken.
    opgelost door npm install require --save --prefix D:/locatie te gebruiken

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ var dal = require('./storage.js');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";   // moet erbij zodat het geen problemen vormt om een self-signed cert te negeren.
 
 
-console.log("Hello World!");
+console.log("Hello World!"); 
